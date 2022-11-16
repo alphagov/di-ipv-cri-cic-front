5120fcf6c6f1ac8ff4d4fc759590839a748f53d9
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2022-11-16 15:05:13 +0000

    Copied over passport expiry screen to build on for BRP screen

--- src/components/journeys/cic/cicRoutes.ts
+++ src/components/journeys/cic/cicRoutes.ts
@@ -4,10 +4,13 @@ import {landingPage} from "./landingPageService";
 import { accessibilityStatement } from "./accessibilityStatementService";
 import { privacyStatement } from "./privacyStatementService";
 import {selectPhotoId, validateSelectPhotoId} from "./photoIdSelection";
+import {dateOfExpiry} from "./passportExpiry";
 
 export const cicRoutes = {
   [`${AUTH_ROOT}${PATH_NAMES.LANDING_PAGE}`]: { "GET": landingPage },
   [`${AUTH_ROOT}${PATH_NAMES.PHOTO_ID_SELECTION}`]: { "GET": selectPhotoId, "POST": validateSelectPhotoId },
+  [`${AUTH_ROOT}${PATH_NAMES.PASSPORT_DETAILS}`]: { "GET": dateOfExpiry },
   [`${PATH_NAMES.PRIVACY_STATEMENT}`]: { "GET": privacyStatement },
   [`${PATH_NAMES.ACCESSIBILITY_STATEMENT}`]: { "GET": accessibilityStatement }
 }
+
