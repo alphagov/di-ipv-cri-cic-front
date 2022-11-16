f1bf94d7ae60c47fe2a5290bf25e4fe4ff77d416
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2022-11-16 17:38:15 +0000

    BRP expiry page written pending function refactor

--- src/components/journeys/cic/brpExpiry.ts
+++ src/components/journeys/cic/brpExpiry.ts
@@ -3,10 +3,10 @@ import { AUTH_ROOT } from '../../../app.constants';
 import { EVENTS, PATH_NAMES } from './app.constants'
 import { getNext } from "./stateMachine/stateMachine";
 
-const TEMPLATE = "passportExpiry.njk"
+const TEMPLATE = "brpExpiry.njk"
 const BACK = `${AUTH_ROOT}${PATH_NAMES.PHOTO_ID_SELECTION}`
 
-export async function passportDateOfExpiry (req: Request, res: Response): Promise<void> {
+export async function brpDateOfExpiry (req: Request, res: Response): Promise<void> {
   res.render(TEMPLATE, {
     back: BACK
   })
--- src/components/journeys/cic/cicRoutes.ts
+++ src/components/journeys/cic/cicRoutes.ts
@@ -4,12 +4,14 @@ import {landingPage} from "./landingPageService";
 import { accessibilityStatement } from "./accessibilityStatementService";
 import { privacyStatement } from "./privacyStatementService";
 import {selectPhotoId, validateSelectPhotoId} from "./photoIdSelection";
-import {dateOfExpiry} from "./passportExpiry";
+import {passportDateOfExpiry} from "./passportExpiry";
+import {brpDateOfExpiry} from "./brpExpiry";
 
 export const cicRoutes = {
   [`${AUTH_ROOT}${PATH_NAMES.LANDING_PAGE}`]: { "GET": landingPage },
   [`${AUTH_ROOT}${PATH_NAMES.PHOTO_ID_SELECTION}`]: { "GET": selectPhotoId, "POST": validateSelectPhotoId },
-  [`${AUTH_ROOT}${PATH_NAMES.PASSPORT_DETAILS}`]: { "GET": dateOfExpiry },
+  [`${AUTH_ROOT}${PATH_NAMES.PASSPORT_DETAILS}`]: { "GET": passportDateOfExpiry },
+  [`${AUTH_ROOT}${PATH_NAMES.BRP_DETAILS}`]: { "GET": brpDateOfExpiry },
   [`${PATH_NAMES.PRIVACY_STATEMENT}`]: { "GET": privacyStatement },
   [`${PATH_NAMES.ACCESSIBILITY_STATEMENT}`]: { "GET": accessibilityStatement }
 }
