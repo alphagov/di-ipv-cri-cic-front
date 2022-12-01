fd5be787f5c48c714d7af9189f93d0f5762b06ae
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2022-12-01 14:26:19 +0000

    Removes old files, I think, and adds APP path for name entry in config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -18,7 +18,8 @@ module.exports = {
       PASSPORT_DETAILS: "/passportDetails",
       BRP_DETAILS: "/brpDetails",
       PHOTOCARD_DL_DETAILS: "/photocardDlDetails",
-      OTHER_PASSPORT_DETAILS: "/otherPassportDetails"
+      OTHER_PASSPORT_DETAILS: "/otherPassportDetails",
+      NAME_ENTRY: "/nameEntry"
     },
     PHOTO_ID_OPTIONS:{
       UK_PASSPORT:"ukPassport",
