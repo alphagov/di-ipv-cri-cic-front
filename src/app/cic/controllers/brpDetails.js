dcc51148e73cc3665a7fddd0d1cf0d7f04d683bf
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2022-12-01 09:11:35 +0000

    Copied F2F-114 files

---
+++ src/app/cic/controllers/brpDetails.js
@@ -0,0 +1,10 @@
+const BaseController = require("hmpo-form-wizard").Controller;
+const DateControllerMixin = require("hmpo-components").mixins.Date;
+
+const DateController = DateControllerMixin(BaseController);
+
+class BrpDetailsController extends DateController {
+
+
+}
+module.exports = BrpDetailsController;
